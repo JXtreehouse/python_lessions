4302c84d78128d4d8c814ba6e51ae47be5e209b3
AlexZ33 <775136985@qq.com>
2018-02-01 16:38:58 +0800

    调用scrapy提供的JsonItemExporter  到出json文件

--- WebCrawler /ArticleSpider/ArticleSpider/pipelines.py
+++ WebCrawler /ArticleSpider/ArticleSpider/pipelines.py
@@ -7,12 +7,14 @@
 import codecs
 import json
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
 
 class ArticlespiderPipeline(object):
     def process_item(self, item, spider):
         return item
 
 class JsonWithEncodingPipeline(object):
+    #自定义json文件的到出
     def __init__(self):
         self.file = codecs.open('article.json', 'w', encoding="utf-8")
     def process_item(self, item, spider):
@@ -21,6 +23,19 @@ class JsonWithEncodingPipeline(object):
         return item
     def spider_closed(self, spider):
         self.file.close()
+
+class JsonExporterPipeline(object):
+    #调用scrapy提供的JsonItemExporter  到出json文件
+    def __init__(self):
+        self.file = open('articleecport.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding="utf-8", ensure_ascii=False)
+        self.exporter.start_exporting()
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+    def process_item(self, item, spider):
+        self.exporter.export_item(item=item)
+        return item
 #图片处理pipline
 class ArticleImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
--- WebCrawler /ArticleSpider/ArticleSpider/settings.py
+++ WebCrawler /ArticleSpider/ArticleSpider/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'ArticleSpider.pipelines.JsonWithEncodingPipeline': 2,
+   'ArticleSpider.pipelines.JsonExporterPipeline': 2,
    #'scrapy.pipelines.images.ImagesPipeline': 2,
    'ArticleSpider.pipelines.ArticleImagePipeline': 1,
 }
