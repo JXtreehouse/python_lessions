eb76b48eeaa92f5dd6ca52749059b4a6b3a3a0dd
AlexZ33 <775136985@qq.com>
2018-02-01 13:43:46 +0800

    简单排序算法 python实现

---
+++ 基础算法/insertionSort.py
@@ -0,0 +1,13 @@
+arr = [2,3,6,5,33,7,23]
+
+def insertionSort(arr):
+    for i in range(len(arr)):
+        preIndex = i-1
+        current = arr[i]
+        while preIndex >= 0 and arr[preIndex] > current:
+            arr[preIndex+1] = arr[preIndex]
+            preIndex-=1
+        arr[preIndex+1] = current
+    return arr
+
+print(insertionSort(arr))
---
+++ 基础算法/mergeSort.py
@@ -0,0 +1,2 @@
+def mergeSort(arr):
+    import
