44e0d9590108c7ddea94ea3ec93691f489ffe150
AlexZ33 <775136985@qq.com>
2020-03-06 18:34:06 +0800

    马赛克

--- PythonBasic/mosaic/mosaic_gen.py
+++ PythonBasic/mosaic/mosaic_gen.py
@@ -1,5 +1,12 @@
+
+# 给图片添加马赛克
 import os
 import numpy as np
+# opencv
+# 安装注意　https://blog.csdn.net/qq_38660394/article/details/80581383
+# pip install opencv-python
 import cv2
 import glob
-import match
+import math
+
+comparison_data_path = ''
